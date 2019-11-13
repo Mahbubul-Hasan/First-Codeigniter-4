05e2840c4045acb48734e0e858a1b504fb9dad14
Mahbubul-Hasan <mahbubul161@gmail.com>
2019-11-14 01:26:13 +0600

    Change insert image folder & user list done

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -5,17 +5,24 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\User;
 use CodeIgniter\Exceptions\PageNotFoundException;
-use SebastianBergmann\CodeCoverage\Report\Html\File;
 
 class UserController extends BaseController
 {
+	protected $user = null;
+	public function __construct()
+	{
+		$this->user = new User();
+	}
+
+
 	// Show a list of user
 	public function index($page = "list")
 	{
 		if (!file_exists(APPPATH . "/Views/users/" . $page . ".php")) {
 			throw new PageNotFoundException($page);
 		}
-		return view('users/' . $page);
+		$data["users"] = $this->user->getUser();
+		return view('users/' . $page, $data);
 	}
 
 	// Show a form For adding a user
@@ -25,18 +32,12 @@ class UserController extends BaseController
 			return view('users/add-user');
 		}
 
-		// var_dump($this->request->getFile("photo"));
-		$photo = $this->request->getFile("photo");;
-		$extension = $photo->getClientExtension();
-		$name = "img_" . date("Ymd_his") . "." . $extension;
-		$photo->move(WRITEPATH . "upload/", $name);
-
 		$validate = $this->validate([
 			'name' => 'required|min_length[3]',
 			'email' => 'required|valid_email',
 			'phone' => 'required|min_length[11]|max_length[13]',
 			'password' => 'required|min_length[6]',
-			// 'photo' => 'uploaded[photo]|max_size[photo,1024]',
+			'photo' => 'uploaded[photo]', 'mime_in[photo,image/jpg,image/jpeg,image/gif,image/png]', 'max_size[photo,4096]',
 		]);
 		if (!$validate) {
 			return view('users/add-user', [
@@ -44,6 +45,12 @@ class UserController extends BaseController
 			]);
 		}
 
+
+		$photo = $this->request->getFile("photo");;
+		$extension = $photo->getClientExtension();
+		$name = "img_" . date("Ymd_his") . "." . $extension;
+		$photo->move("upload/", $name);
+		
 		$request = [
 			"name" => $this->request->getPost("name"),
 			"email" => $this->request->getPost("email"),
--- app/Views/users/list.php
+++ app/Views/users/list.php
@@ -1,1 +1,60 @@
 <!-- Avalon Hosting Services -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h2>User List</h2>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Password</th>
+            <th>Image</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach ($users as $user) { ?>
+            <tr>
+                <td><?= $user["id"] ?></td>
+                <td><?= $user["name"] ?></td>
+                <td><?= $user["email"] ?></td>
+                <td><?= $user["phone"] ?></td>
+                <td><?= $user["password"] ?></td>
+                <td>
+                    <img src="<?= $user["photo"] ?>" alt="" width="80">
+                </td>
+                <td>
+                    <a href="">Edit</a>
+                    <a href="">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+
+</body>
+
+</html>
